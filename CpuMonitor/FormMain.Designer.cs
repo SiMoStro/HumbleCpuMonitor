ade9bb900388173c4e17c5a709b70db0b74e8523
The Central Srutinizer <SiMoStro@users.noreply.github.com>
2019-02-10 18:39:36 +0100

    Check-in
    First commit

---
+++ CpuMonitor/FormMain.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace CpuMonitor
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 173);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1280, 220);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(320, 220);
+            this.Name = "FormMain";
+            this.ShowInTaskbar = false;
+            this.Text = "Humble CPU Monitor";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ CpuMonitor/FormMain.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Reflection;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace CpuMonitor
+{
+    public partial class FormMain : Form
+    {
+        #region [private]
+
+        private NotifyIcon _trayIcon;
+        private PerformanceCounter _theCPUCounter;
+        private bool _allowsHowDisplay = false;
+        private Icon[] _icons;
+        private float _cpuUsage;
+        private int _cpuIconIndex;
+        private const int StepSize = 7;
+
+        private ContextMenu _menu;
+        private MenuItem _exitMenu;
+        private MenuItem _toggleShowhideMenu;
+        private MenuItem _updInsane;
+        private MenuItem _updHalfSecond;
+        private MenuItem _updOneSecond;
+        private MenuItem _updTwoSeconds;
+        private MenuItem _updThreeSeconds;
+
+        private MiniChart _miniChart;
+        private bool _internalExit;
+        private Timer _timer;
+
+        #endregion
+
+        public FormMain()
+        {
+            InitializeComponent();
+            _icons = new Icon[15];
+            _miniChart = new MiniChart();
+            _miniChart.Dock = DockStyle.Fill;
+            Controls.Add(_miniChart);
+
+            Application.ApplicationExit += HandleApplicationExit;
+
+            _theCPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            _trayIcon = new NotifyIcon();
+            _trayIcon.DoubleClick += (o, e) => ToggleWindowVisibility();
+
+            LoadIcons();
+            BuildMenu();
+            UpdateTrayIcon();
+
+            _timer = new Timer { Interval = 1000 };
+            _timer.Tick += HandleTick;
+            _timer.Start();
+        }
+
+        private void LoadIcons()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();
+            for (int i = 0; i < 15; i++)
+            {
+                string icoName = "CpuMonitor.ICOs." + i.ToString("00") + "-ico.ico";
+                Stream ico = assembly.GetManifestResourceStream(icoName);
+                Icon icon = new Icon(ico);
+                _icons[i] = icon;
+            }
+        }
+
+        private void BuildMenu()
+        {
+            _menu = new ContextMenu();
+
+            _exitMenu = new MenuItem("Exit");
+            _exitMenu.Click += (o, e) =>
+            {
+                _internalExit = true;
+                Application.Exit();
+            };
+
+            _toggleShowhideMenu = new MenuItem();
+            _toggleShowhideMenu.Click += (o, e) => ToggleWindowVisibility();
+
+
+
+            MenuItem upd = new MenuItem("Update Interval");
+            _updInsane = new MenuItem("1/4 second");
+            _updInsane.Click += (o, e) => _timer.Interval = 250;
+            _updHalfSecond = new MenuItem("1/2 second");
+            _updHalfSecond.Click += (o, e) => _timer.Interval = 500;
+            _updOneSecond = new MenuItem("1 second");
+            _updOneSecond.Click += (o, e) => _timer.Interval = 1000;
+            _updTwoSeconds = new MenuItem("2 second");
+            _updTwoSeconds.Click += (o, e) => _timer.Interval = 2000;
+            _updThreeSeconds = new MenuItem("3 second");
+            _updThreeSeconds.Click += (o, e) => _timer.Interval = 3000;
+            upd.MenuItems.Add(_updInsane);
+            upd.MenuItems.Add(_updHalfSecond);
+            upd.MenuItems.Add(_updOneSecond);
+            upd.MenuItems.Add(_updTwoSeconds);
+            upd.MenuItems.Add(_updThreeSeconds);
+
+            _menu.MenuItems.Add(_toggleShowhideMenu);
+            _menu.MenuItems.Add(upd);
+            _menu.MenuItems.Add(_exitMenu);
+            _menu.Popup += (o, e) =>
+            {
+                _toggleShowhideMenu.Text = Visible ? "Hide CPU chart" : "Show CPU chart";
+            };
+            _trayIcon.ContextMenu = _menu;
+        }
+
+        private void ToggleWindowVisibility()
+        {
+            _allowsHowDisplay = true;
+            if (Visible) Hide();
+            else Show();
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            base.SetVisibleCore(_allowsHowDisplay ? value : _allowsHowDisplay);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (!_internalExit) e.Cancel = true;
+            base.OnClosing(e);
+            Hide();
+        }
+
+        private void UpdateTrayIcon()
+        {
+            if(_trayIcon.Icon != _icons[_cpuIconIndex]) _trayIcon.Icon = _icons[_cpuIconIndex];
+            if(!_trayIcon.Visible) _trayIcon.Visible = true;
+        }
+
+        private void HandleApplicationExit(object sender, EventArgs e)
+        {
+            _trayIcon.Visible = false;
+        }
+
+        private void HandleTick(object sender, EventArgs e)
+        {
+            _cpuUsage = _theCPUCounter.NextValue();
+            _miniChart.AddValue(_cpuUsage);
+            _cpuIconIndex = (int)Math.Floor(_cpuUsage / 7);
+            if (_cpuIconIndex > 14) _cpuIconIndex = 14;
+            _trayIcon.Text = (_cpuUsage / 100).ToString("P");
+            UpdateTrayIcon();
+        }
+    }
+}
